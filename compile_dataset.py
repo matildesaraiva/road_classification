17b876d594b84cd7cf187af556f261edc7b40a94
matildesaraiva <matildesaraiva99@gmal.com>
2023-08-21 12:03:48 +0100

    Creation of train and test datasets

--- compile_dataset.py
+++ compile_dataset.py
@@ -38,7 +38,30 @@ images = np.array(images)
 masks = np.array(masks)
 masks.shape
 
-# Create TRAIN dataset
+# Create datasets (train and test)
+# Shuffle the data
+data_size = len(images)
+indices = np.arange(data_size)
+np.random.shuffle(indices)
+
+# Split data into train and test
+split_ratio = 0.8  # 80% for training, 20% for testing
+split_index = int(data_size * split_ratio)
+
+train_indices = indices[:split_index]
+test_indices = indices[split_index:]
+
+train_images = [images[i] for i in train_indices]
+train_masks = [masks[i] for i in train_indices]
+
+test_images = [images[i] for i in test_indices]
+test_masks = [masks[i] for i in test_indices]
+
+# Save the datasets
 with h5py.File("C:/data/Dataset_train.h5", 'w') as hdf:
-    hdf.create_dataset('images', data=images, compression='gzip', compression_opts=9)
-    hdf.create_dataset('masks', data=masks, compression='gzip', compression_opts=9)
+    hdf.create_dataset('images', data=train_images, compression='gzip', compression_opts=9)
+    hdf.create_dataset('masks', data=train_masks, compression='gzip', compression_opts=9)
+
+with h5py.File("C:/data/Dataset_test.h5", 'w') as hdf:
+    hdf.create_dataset('images', data=test_images, compression='gzip', compression_opts=9)
+    hdf.create_dataset('masks', data=test_masks, compression='gzip', compression_opts=9)
